5dff192b27c6401fd55801e3c69ddf7dc0305a3f
Glraey.Laptop <mathematrix@foxmail.com>
2014-01-08 13:57:27 +0800

    test wsgi-based url dispatch

---
+++ WsgiUrlDispatch.py
@@ -0,0 +1,41 @@
+from wsgiref.simple_server import make_server
+
+URL_PATTERNS= (
+    ('hi/','say_hi'),
+    ('hello/','say_hello'),
+    )
+
+class Dispatcher(object):
+
+    def _match(self,path):
+        path = path.split('/')[1]
+        for url,app in URL_PATTERNS:
+            if path in url:
+                return app
+
+    def __call__(self,environ, start_response):
+        path = environ.get('PATH_INFO','/')
+        print 'path:' + str(path)
+        app = self._match(path)
+        if app :
+            print 'globals():' + str(globals())
+            app = globals()[app]
+            return app(environ, start_response)
+        else:
+            start_response("404 NOT FOUND",[('Content-type', 'text/plain')])
+            return ["Page dose not exists!"]
+
+def say_hi(environ, start_response):
+    start_response("200 OK",[('Content-type', 'text/html')])
+    return ["kenshin say hi to you!"]
+
+def say_hello(environ, start_response):
+    start_response("200 OK",[('Content-type', 'text/html')])
+    return ["kenshin say hello to you!"]
+
+app = Dispatcher()
+
+httpd = make_server('', 8002, app)
+print "Serving on port 8002..."
+httpd.serve_forever()
+
