355d937e3032e69ed9a1860ea9b8dba9a0b4c175
damon-devops <damon.devops@gmail.com>
2014-05-22 22:19:06 +0800

    test routes
    Finally I understand routes' usage in Neutron, I haven't find
    Controller in wsgi.py! My fault!

--- TestRoutes1.py
+++ TestRoutes1.py
@@ -3,6 +3,7 @@
 
 import routes
 import webob.dec
+import routes.middleware
 from wsgiref.simple_server import make_server
 
 class controller(object):
@@ -30,31 +31,34 @@ class controller(object):
         return "do list_many()"
     def delete_many(self):
         return "do delete_many()"
+
 class appclass(object):
 
     def __init__(self):
         a = controller()
-        map = Mapper()
+        map = routes.Mapper()
         """路由匹配条件1"""
-        #map.connect('/images',controller=a,
-        #           action='search',
-        #           conditions={'method':['GET']})
+        map.connect('/images',controller=a,
+                  action='search',
+                  conditions={'method':['GET']})
         """路由匹配条件2"""
-        #map.connect('name',"/{action}/{pid}",controller=a)
+        map.connect('name',"/{action}/{pid}",controller=a)
         """路由匹配条件3"""
-        #map.resource("message","messages",controller=a,collection={'search':'GET'})
+        map.resource("message","messages",controller=a,collection={'search':'GET'})
         """路由匹配条件4"""
-        #map.resource('message', 'messages',controller=a,
-                        #collection={'list_many':'GET','create_many':'POST'},
-                        #member={'update_many':'POST','delete_many':'POST'})
+        map.resource('message', 'messages',controller=a,
+                        collection={'list_many':'GET','create_many':'POST'},
+                        member={'update_many':'POST','delete_many':'POST'})
         """路由匹配条件5"""
         map.resource('message', 'messages',controller=a,path_prefix='/{projectid}',
                     collection={'list_many':'GET','create_many':'POST'},
                     member={'update_many':'POST','delete_many':'POST'})
-        self.route = middleware.RoutesMiddleware(self.dispatch,map)
+        self.route = routes.middleware.RoutesMiddleware(self.dispatch,map)
+
     @webob.dec.wsgify
     def __call__(self,req):
         return self.route
+
     @staticmethod
     @webob.dec.wsgify
     def dispatch(req):
@@ -76,4 +80,4 @@ class appclass(object):
 if __name__=="__main__":
     app = appclass()
     server = make_server('',8088,app)
-    server.serve_forever
+    server.serve_forever()
